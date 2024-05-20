93f28feb66020bc0fd9c0271e4988af4336d2765
bekalu73 <bekalusisay2010@gmail.com>
2024-05-20 10:35:51 +0300

    complete project set up and react-router-dom set up

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<Home />} />
+          <Route path="other" element={<div>Hello</div>} />
+        </Route>
+        <Route path="*" element={<div>Page Not Found!</div>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
