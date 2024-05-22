bc53ff34c7dae62193e93762ace618d0b18449b9
bekalu73 <bekalusisay2010@gmail.com>
2024-05-22 09:48:34 +0300

    add contact  and product component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,108 @@
+import React, { useRef } from "react";
+import "./Contact.css";
+import { FaPhone } from "react-icons/fa6";
+import { TfiEmail } from "react-icons/tfi";
+import { FaMobileScreenButton } from "react-icons/fa6";
+import { PiPhoneCallFill } from "react-icons/pi";
+import { MdLocationPin } from "react-icons/md";
+import { FaArrowAltCircleRight } from "react-icons/fa";
+
+function Contact() {
+  return (
+    <section className="contact">
+      <div className="contact__container">
+        <h2>Contact Us</h2>
+        <h5>Get In Touch</h5>
+        <div className="contact__option">
+          <div className="contact__option--info">
+            <div className="contact__option--number">
+              <p>
+                <FaPhone />
+                Telephone: <span>+251118218121</span>
+              </p>
+              <p>
+                <FaMobileScreenButton />
+                Mobile: <span> +251911-74 35 97, +251912-05 22 33</span>
+              </p>
+            </div>
+            <div className="contact__option--email">
+              <TfiEmail />
+              Email:
+              <p>
+                <span>
+                  <FaArrowAltCircleRight />
+                  info@retina-pharma.com
+                </span>
+              </p>
+              <p>
+                <span>
+                  <FaArrowAltCircleRight />
+                  deme@retina-pharma.com
+                </span>
+              </p>
+              <p>
+                <span>
+                  <FaArrowAltCircleRight />
+                  retinams@gmail.com
+                </span>
+              </p>
+            </div>
+            <div className="contact__option--callcenter">
+              <PiPhoneCallFill />
+              Call Center:
+              <p>
+                <span>
+                  <FaArrowAltCircleRight />
+                  +2518218121 (working hours)
+                </span>
+              </p>
+              <p>
+                <span>
+                  <FaArrowAltCircleRight />
+                  +251911938611 (off working hours including weekends &
+                  holidays)
+                </span>
+              </p>
+            </div>
+            <div className="contact__option--address">
+              <MdLocationPin />
+              Address:
+              <p>
+                <span>
+                  Bole Subcity, Woreda 03, Yoki Building H.No.724-201, around
+                  Adey Ababa stadium.
+                </span>
+              </p>
+            </div>
+          </div>
+          {/* END OF CONTACT OPTIONS */}
+          <form>
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter Your Full Name"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter Your Full email"
+              required
+            />
+            <textarea
+              name="message"
+              rows="7"
+              placeholder="Leave Your Message Here"
+              required
+            ></textarea>
+            <button type="submit" className="btn btn-primary">
+              Send Message
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -44,28 +44,28 @@ function Product() {
           <div className="product__item">
             <div className="product__img">
               <img
-                src="https://retina-pharma.com/wp-content/uploads/2015/05/Picture2-150x150.png"
+                src="https://retina-pharma.com/wp-content/uploads/2015/05/Picture3-150x150.png"
                 alt=""
               />
             </div>
-            <div className="product__title">Diagnostics and Life Sciences</div>
+            <div className="product__title">Ophthalmic Products</div>
             <div className="product__list">
               <ul>
                 <li>
                   <IoCheckmarkDoneSharp size={25} />
-                  Medical Laboratory Equipment
+                  Ophthalmic Consumables
                 </li>
                 <li>
                   <IoCheckmarkDoneSharp size={25} />
-                  Medical Laboratory Reagents
+                  Ophthalmic Surgical Instruments
                 </li>
                 <li>
                   <IoCheckmarkDoneSharp size={25} />
-                  Test Kits
+                  Ophthalmic Equipment
                 </li>
                 <li>
                   <IoCheckmarkDoneSharp size={25} />
-                  Scientific and Research tools and kits
+                  Eye drops
                 </li>
               </ul>
             </div>
@@ -74,28 +74,16 @@ function Product() {
           <div className="product__item">
             <div className="product__img">
               <img
-                src="https://retina-pharma.com/wp-content/uploads/2015/05/Picture2-150x150.png"
+                src="https://retina-pharma.com/wp-content/uploads/2015/05/Picture4-150x150.png"
                 alt=""
               />
             </div>
-            <div className="product__title">Diagnostics and Life Sciences</div>
+            <div className="product__title">Water Purification System</div>
             <div className="product__list">
               <ul>
                 <li>
                   <IoCheckmarkDoneSharp size={25} />
-                  Medical Laboratory Equipment
-                </li>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Medical Laboratory Reagents
-                </li>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Test Kits
-                </li>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Scientific and Research tools and kits
+                  Laboratory grade water purifiers
                 </li>
               </ul>
             </div>
@@ -104,28 +92,19 @@ function Product() {
           <div className="product__item">
             <div className="product__img">
               <img
-                src="https://retina-pharma.com/wp-content/uploads/2015/05/Picture2-150x150.png"
+                src="https://retina-pharma.com/wp-content/uploads/2015/05/Picture5-150x150.png"
                 alt=""
               />
             </div>
-            <div className="product__title">Diagnostics and Life Sciences</div>
+            <div className="product__title">
+              Tender Management and Trade Auxiliary
+            </div>
             <div className="product__list">
               <ul>
                 <li>
                   <IoCheckmarkDoneSharp size={25} />
-                  Medical Laboratory Equipment
-                </li>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Medical Laboratory Reagents
-                </li>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Test Kits
-                </li>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Scientific and Research tools and kits
+                  With the Ethiopian Pharmaceutical Supply Agency and other big
+                  procuring agencies
                 </li>
               </ul>
             </div>
