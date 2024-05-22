1d9b36294079e15832dcbb83651a90e73cb157b5
bekalu73 <bekalusisay2010@gmail.com>
2024-05-22 15:21:45 +0300

    add about card component

--- src/components/About/AboutCard/AboutCard.jsx
+++ src/components/About/AboutCard/AboutCard.jsx
@@ -1,115 +1,71 @@
 import React from "react";
 import "./AboutCard.css";
+import { IoCheckmarkDoneSharp } from "react-icons/io5";
+import { PiTarget } from "react-icons/pi";
+import { FaEye } from "react-icons/fa";
+import { FaHandHolding } from "react-icons/fa";
 
 function AboutCard() {
   return (
     <div className="aboutCard">
       <div className="aboutCard__container">
         <div className="aboutCards__header">
-          <h2>aboutCards</h2>
-          <p>Major Line of Business</p>
+          <h2>Mission, Vision and Values</h2>
+          {/* <p>Major Line of Business</p> */}
         </div>
         <div className="aboutCard__container--item">
           <div className="aboutCard__item">
             <div className="aboutCard__img">
-              <img
-                src="https://retina-pharma.com/wp-content/uploads/2015/05/Picture2-150x150.png"
-                alt=""
-              />
+              <PiTarget size={70} />
             </div>
-            <div className="aboutCard__title">
-              Diagnostics and Life Sciences
-            </div>
+            <div className="aboutCard__title">Mission</div>
             <div className="aboutCard__list">
-              <ul>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Medical Laboratory Equipment
-                </li>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Medical Laboratory Reagents
-                </li>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Test Kits
-                </li>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Scientific and Research tools and kits
-                </li>
-              </ul>
+              <p>
+                We are committed to the prosperity of our customers, fellow
+                employees, our community, and our company; we build strong
+                partnerships with all of them.
+              </p>
             </div>
-            <button className="aboutCard__btn">Contact Us</button>
           </div>
           <div className="aboutCard__item">
             <div className="aboutCard__img">
-              <img
-                src="https://retina-pharma.com/wp-content/uploads/2015/05/Picture3-150x150.png"
-                alt=""
-              />
+              <FaEye size={70} />
             </div>
-            <div className="aboutCard__title">Ophthalmic aboutCards</div>
+            <div className="aboutCard__title">Vision</div>
             <div className="aboutCard__list">
-              <ul>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Ophthalmic Consumables
-                </li>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Ophthalmic Surgical Instruments
-                </li>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Ophthalmic Equipment
-                </li>
-                <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Eye drops
-                </li>
-              </ul>
+              We see our selves being a leading provider of quality diagnostics
+              and life sciences solutions in Ethiopia and the Horn of Africa
             </div>
-            <button className="aboutCard__btn">Contact Us</button>
           </div>
           <div className="aboutCard__item">
             <div className="aboutCard__img">
-              <img
-                src="https://retina-pharma.com/wp-content/uploads/2015/05/Picture4-150x150.png"
-                alt=""
-              />
+              <FaHandHolding size={70} />
             </div>
-            <div className="aboutCard__title">Water Purification System</div>
+            <div className="aboutCard__title">Core Values</div>
             <div className="aboutCard__list">
               <ul>
                 <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  Laboratory grade water purifiers
+                  <IoCheckmarkDoneSharp />
+                  Quality Obsession
                 </li>
-              </ul>
-            </div>
-            <button className="aboutCard__btn">Contact Us</button>
-          </div>
-          <div className="aboutCard__item">
-            <div className="aboutCard__img">
-              <img
-                src="https://retina-pharma.com/wp-content/uploads/2015/05/Picture5-150x150.png"
-                alt=""
-              />
-            </div>
-            <div className="aboutCard__title">
-              Tender Management and Trade Auxiliary
-            </div>
-            <div className="aboutCard__list">
-              <ul>
                 <li>
-                  <IoCheckmarkDoneSharp size={25} />
-                  With the Ethiopian Pharmaceutical Supply Agency and other big
-                  procuring agencies
+                  <IoCheckmarkDoneSharp />
+                  Excellence of service
+                </li>
+                <li>
+                  <IoCheckmarkDoneSharp />
+                  Flexibility in partnership
+                </li>
+                <li>
+                  <IoCheckmarkDoneSharp />
+                  Ethical business-standard
+                </li>
+                <li>
+                  <IoCheckmarkDoneSharp />
+                  Mutual benefit
                 </li>
               </ul>
             </div>
-            <button className="aboutCard__btn">Contact Us</button>
           </div>
         </div>
       </div>
