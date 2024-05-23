11f45566e0f02a7fe68a3b90412d68bafefbb3ff
bekalu73 <bekalusisay2010@gmail.com>
2024-05-23 11:29:08 +0300

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Header.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/Retina-pharmaceuticals-logo.png";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
@@ -10,6 +10,7 @@ function Header() {
   const handleLinkClick = () => {
     setOpen(false);
   };
+
   return (
     <div className="header">
       <div className="header__container">
@@ -19,29 +20,91 @@ function Header() {
           </Link>
         </div>
         <div className={`header__links ${isOpen ? "header__links--open" : ""}`}>
-          <Link to="/" onClick={handleLinkClick} className="underlined">
-            Home
-          </Link>
-          <Link to="/about" onClick={handleLinkClick} className="underlined">
-            About
-          </Link>
-          <Link
-            to="/customers"
-            onClick={handleLinkClick}
-            className="underlined"
-          >
-            Customers
-          </Link>
-          <Link to="/support" onClick={handleLinkClick} className="underlined">
-            Technical Support
-          </Link>
-          <Link
+          <div className="header__dropdown">
+            <NavLink
+              to="/"
+              onClick={handleLinkClick}
+              className={({ isActive }) =>
+                `underlined ${isActive ? "active" : ""}`
+              }
+            >
+              Home
+            </NavLink>
+          </div>
+
+          <div className="header__dropdown">
+            <NavLink
+              to="/about"
+              onClick={handleLinkClick}
+              className={({ isActive }) =>
+                `underlined ${isActive ? "active" : ""}`
+              }
+            >
+              About
+            </NavLink>
+            <div className="dropdown__menu">
+              <Link to="/about" onClick={handleLinkClick}>
+                Mission
+              </Link>
+              <Link to="/about" onClick={handleLinkClick}>
+                Vision
+              </Link>
+              <Link to="/about" onClick={handleLinkClick}>
+                Core Values
+              </Link>
+              <Link to="/about" onClick={handleLinkClick}>
+                Efficient Structure
+              </Link>
+            </div>
+          </div>
+
+          <div className="header__dropdown">
+            <NavLink
+              to="/customers"
+              onClick={handleLinkClick}
+              className={({ isActive }) =>
+                `underlined ${isActive ? "active" : ""}`
+              }
+            >
+              Customers
+            </NavLink>
+            <div className="dropdown__menu">
+              <Link to="/customers" onClick={handleLinkClick}>
+                Our Customers
+              </Link>
+              <Link to="/customers" onClick={handleLinkClick}>
+                Partners
+              </Link>
+            </div>
+          </div>
+
+          <div className="header__dropdown">
+            <NavLink
+              to="/support"
+              onClick={handleLinkClick}
+              className={({ isActive }) =>
+                `underlined ${isActive ? "active" : ""}`
+              }
+            >
+              Technical Support
+            </NavLink>
+            {/* <div className="dropdown__menu">
+              <Link to="/sub-support-1" onClick={handleLinkClick}>
+                Sub Support 1
+              </Link>
+              <Link to="/sub-support-2" onClick={handleLinkClick}>
+                Sub Support 2
+              </Link>
+            </div> */}
+          </div>
+
+          <NavLink
             className="header__links-contact"
             to="/contact"
             onClick={handleLinkClick}
           >
             Contact Us
-          </Link>
+          </NavLink>
         </div>
         <div className="header__hamburger">
           {isOpen ? (
